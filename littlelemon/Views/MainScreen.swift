a18a01c9d2593d082d68247b9c28f0250d0b0796
Staxar <marcin.kepno@gmail.com>
2024-12-19 14:31:11 +0100

    meta

---
+++ littlelemon/Views/MainScreen.swift
@@ -0,0 +1,27 @@
+//
+//  MainScreen.swift
+//  littlelemon
+//
+//  Created by Marcin Domagała - 19.12.2024
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Header()
+                Menu()
+            }
+        }
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
